60ec2a6af2353b88c5b80082d1058fc997fc74e7
Ivan Esin <ivan.esin.78@mail.ru>
2024-03-06 18:42:28 +0400

    My homework

---
+++ Занятие3/script.js
@@ -0,0 +1,33 @@
+// function f(money) {
+//     let result = (money * 0.87) * 0.75
+//     return result;
+// };
+
+
+// const userMoney = +prompt();
+
+// let moneyMonth = f(userMoney);
+
+// console.log(f(userMoney))
+// console.log(f(moneyMonth*0.3))
+
+// function sayhi() {
+//     alert('Привет')
+//     alert('Нажали на кнопку')
+//     return 5;
+// }
+
+
+// function qustion(qustion, answer) {
+//     const userAnswer = prompt(qustion).toLowerCase();
+//     if (userAnswer === answer) {
+//         alert('Правильно!');
+//     } else {
+//         alert('Не правильно!');
+//     };
+// };
+
+// function puzzle() {
+//     qustion('Зимой и летом одним цветом', 'ёлка');
+//     qustion('Дед во сто шуб одет', 'лук')
+// };
